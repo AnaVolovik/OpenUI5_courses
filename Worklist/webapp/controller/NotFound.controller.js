5482e77b5935ec6144e99e6aea1bae4cd563be0f
AnaVolovik <nastasia.volovik@gmail.com>
2024-10-10 16:49:05 +0300

    Clear the code of unnecessary comments

--- Worklist/webapp/controller/NotFound.controller.js
+++ Worklist/webapp/controller/NotFound.controller.js
@@ -1,19 +1,15 @@
 sap.ui.define([
-		"zjblessons/Worklist/controller/BaseController"
-	], function (BaseController) {
-		"use strict";
+	"zjblessons/Worklist/controller/BaseController"
+], function (BaseController) {
+	"use strict";
 
-		return BaseController.extend("zjblessons.Worklist.controller.NotFound", {
+	return BaseController.extend("zjblessons.Worklist.controller.NotFound", {
 
-			/**
-			 * Navigates to the worklist when the link is pressed
-			 * @public
-			 */
-			onLinkPressed : function () {
-				this.getRouter().navTo("worklist");
-			}
+		onLinkPressed : function () {
+			this.getRouter().navTo("worklist");
+		}
 
-		});
+	});
 
-	}
+}
 );
