5d4f189df2173773d5c03c7c31bdd6d3a0e33100
AnaVolovik <nastasia.volovik@gmail.com>
2024-10-12 18:53:35 +0300

    Copy Worklist from Lab_2

---
+++ Worklist/webapp/Component.js
@@ -0,0 +1,48 @@
+/* global document */
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/Device",
+	"zjblessons/Worklist/model/models",
+	"zjblessons/Worklist/controller/ErrorHandler"
+], function (UIComponent, Device, models, ErrorHandler) {
+	"use strict";
+
+	return UIComponent.extend("zjblessons.Worklist.Component", {
+
+		metadata : {
+			manifest: "json"
+		},
+
+		init : function () {
+			UIComponent.prototype.init.apply(this, arguments);
+
+			this._oErrorHandler = new ErrorHandler(this);
+
+			this.setModel(models.createDeviceModel(), "device");
+
+			this.getRouter().initialize();
+		},
+
+		destroy : function () {
+			this._oErrorHandler.destroy();
+			UIComponent.prototype.destroy.apply(this, arguments);
+		},
+
+		getContentDensityClass : function() {
+			if (this._sContentDensityClass === undefined) {
+				if (jQuery(document.body).hasClass("sapUiSizeCozy") || jQuery(document.body).hasClass("sapUiSizeCompact")) {
+					this._sContentDensityClass = "";
+				} else if (!Device.support.touch) {
+					this._sContentDensityClass = "sapUiSizeCompact";
+				} else {
+					// "cozy" in case of touch support; default for most sap.m controls, but needed for desktop-first controls like sap.ui.table.Table
+					this._sContentDensityClass = "sapUiSizeCozy";
+				}
+			}
+			return this._sContentDensityClass;
+		}
+
+	});
+
+}
+);
